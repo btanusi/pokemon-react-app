df3ae6ac610536fd9cd867b5da9382c526831926
Brianna Tanusi <btanusi@gmail.com>
2020-11-06 12:14:12 -0800

    cleaned up comments

--- src/App.js
+++ src/App.js
@@ -29,80 +29,80 @@ class App extends React.Component {
       this.getPokemonInfo();
     }
 
-    getPokemonInfo = async ()=> {
-      let pokemonInputName = this.state.inputName;    
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInputName}`)
-      const json = await response.json()
-      //some sort of error checking if json === "Not Found"
-      let pokemonObj = {name: json.name, type: json.types[0].type.name, baseXP: json.base_experience}
-      //let pokemonProperties = [json.name, JSON.stringify(json.types)]
-      //json.types.forEach(type => pokemonProperties.push(type.name))
-      //let newPokemonArray = this.state.pokemonArray.slice()
-      //newPokemonArray.push(pokemonProperties)
-      this.setState({
-                      pokemonArray: this.state.pokemonArray.concat(pokemonObj),
-                      searchFlag: true
-                    })
-    }
+  getPokemonInfo = async ()=> {
+    let pokemonInputName = this.state.inputName;    
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInputName}`)
+    const json = await response.json()
+    //some sort of error checking if json === "Not Found"
+    let pokemonObj = {name: json.name, type: json.types[0].type.name, baseXP: json.base_experience}
+    //let pokemonProperties = [json.name, JSON.stringify(json.types)]
+    //json.types.forEach(type => pokemonProperties.push(type.name))
+    //let newPokemonArray = this.state.pokemonArray.slice()
+    //newPokemonArray.push(pokemonProperties)
+    this.setState({
+                    pokemonArray: this.state.pokemonArray.concat(pokemonObj),
+                    searchFlag: true
+                  })
+  }
 
 
-    handleOnListAllPokemonNames = async (event) => {
-      event.preventDefault();
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
-      const json = await response.json();
+  handleOnListAllPokemonNames = async (event) => {
+    event.preventDefault();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+    const json = await response.json();
 
-      let allPokemon = json.results.map(e => e.name);
-      
-      this.setState({
-                      allPokemonNames: allPokemon
-                    })
+    let allPokemon = json.results.map(e => e.name);
+    
+    this.setState({
+                    allPokemonNames: allPokemon
+                  })
 
-    }
+  }
 
-    onListClickPokemon(pokeName)
-    {
-      this.setState({inputName: pokeName}, this.getPokemonInfo)
-    }
+  onListClickPokemon(pokeName)
+  {
+    this.setState({inputName: pokeName}, this.getPokemonInfo)
+  }
 
-    onListSimilarTypes = async () =>
-    {
-      for(let i = 0; i < this.state.pokemonArray.length; i++){
-        let pokemonObj = this.state.pokemonArray[i];
-        if(pokemonObj.name === this.state.inputName){
-          const response = await fetch(`https://pokeapi.co/api/v2/type/${pokemonObj.type}`)
-          const json = await response.json();
-          this.setState({similarPokemonNames: json.pokemon.map(element => element.pokemon.name)})
-        }
+  onListSimilarTypes = async () =>
+  {
+    for(let i = 0; i < this.state.pokemonArray.length; i++){
+      let pokemonObj = this.state.pokemonArray[i];
+      if(pokemonObj.name === this.state.inputName){
+        const response = await fetch(`https://pokeapi.co/api/v2/type/${pokemonObj.type}`)
+        const json = await response.json();
+        this.setState({similarPokemonNames: json.pokemon.map(element => element.pokemon.name)})
       }
     }
+  }
 
-    onCollectPokemon = () =>
-    {
-      alert(`You caught ${this.state.pokemonArray[0].name}!`)
-      this.setState(
-        {pokemonCollection: this.state.pokemonCollection.concat(this.state.pokemonArray[0])}
-      )
-    }
+  onCollectPokemon = () =>
+  {
+    alert(`You caught ${this.state.pokemonArray[0].name}!`)
+    this.setState(
+      {pokemonCollection: this.state.pokemonCollection.concat(this.state.pokemonArray[0])}
+    )
+  }
 
-    onClickHome = () =>{
-      this.setState(
-        {
-          searchFlag: false,
-          pokemonArray: [],
-          allPokemonNames: [],
-          similarPokemonNames: [],
-          viewCollectionFlag: false,
-        }
-      )
-    }
+  onClickHome = () =>{
+    this.setState(
+      {
+        searchFlag: false,
+        pokemonArray: [],
+        allPokemonNames: [],
+        similarPokemonNames: [],
+        viewCollectionFlag: false,
+      }
+    )
+  }
 
-    onViewCollection = () => {
-      this.setState(
-        {
-          viewCollectionFlag: true
-        }
-      )
-    }
+  onViewCollection = () => {
+    this.setState(
+      {
+        viewCollectionFlag: true
+      }
+    )
+  }
     
   render() {
     //render pokemonArray if we've searched the pokemon
